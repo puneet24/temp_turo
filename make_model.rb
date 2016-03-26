1e9d02107d7c5599b71397ee7f1391df20abf587
punsa <puneet.241994.agarwal@gmail.com>
2016-03-26 16:23:36 +0530

    few

--- make_model.rb
+++ make_model.rb
@@ -29,7 +29,7 @@ res["hits"]["hits"].each do |r|
 end
 
 @make_and_model.each do |key,value|
-	puts '{"mod" : "' + key + '", "cnt" : "' + value + '"},'
+	puts '{"mod" : "' + key.to_s + '", "cnt" : "' + value.to_s + '"},'
 end
 
 #puts @make_and_model.keys
